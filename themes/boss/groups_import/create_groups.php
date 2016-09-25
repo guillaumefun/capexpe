176b05ff7aa47a793f9542ce9cdfa71aed8f7ba5
guillaumefun <guillaume.funck@gmail.com>
2016-09-25 08:28:54 +0200

    Using SQL request instead of groups_add_groupmeta to add group metadata

--- themes/boss/groups_import/create_groups.php
+++ themes/boss/groups_import/create_groups.php
@@ -54,7 +54,7 @@ for($i = 1 ; $i < $x ; $i++ ) {
   echo $title . '<br>';
 
   // cherche l'id de la page de groupe parent en cherchant parmis les page de groupe la page dont le nom est le même que le nom de l'expé
-  for( $i = 1000200 ; $i < 1000350 ; $i++){
+  for( $i = 2000 ; $i < 1000350 ; $i++){
     // echo get_the_title($i) . ' <br>';
      if( get_the_title($i) == $title){
          $post_id = $i;
@@ -65,8 +65,15 @@ for($i = 1 ; $i < $x ; $i++ ) {
 echo '<br><br>' . $post_id;
 
   // ajoute une méta pour lié le blog avec la page de groupe parent
-  groups_add_groupmeta( $id, 'bpge', 'a:7:{s:8:"gpage_id";i:' . $post_id . ';s:19:"display_page_layout";s:7:"profile";s:12:"display_page";s:6:"public";s:17:"display_page_name";s:6:"Extras";s:10:"gpage_name";s:5:"Pages";s:14:"display_gpages";s:6:"public";s:9:"home_name";s:4:"Home";}' );
+  // groups_add_groupmeta( $id, 'bpge', 'a:7:{s:8:"gpage_id";i:3001;s:19:"display_page_layout";s:7:"profile";s:12:"display_page";s:6:"public";s:17:"display_page_name";s:6:"Extras";s:10:"gpage_name";s:5:"Pages";s:14:"display_gpages";s:6:"public";s:9:"home_name";s:4:"Home";}' );
 
+  $db = new PDO('mysql:host=localhost;dbname=wordpress2;charset=utf8', 'root' , 'root' );
+  $req = $db -> prepare(' INSERT INTO wp_bp_groups_groupmeta(group_id, meta_key, meta_value) VALUES (:group_id, :meta_key, :meta_value) ');
+  $req -> execute( array(
+    'group_id' => $id, 
+    'meta_key' => 'bpge', 
+    'meta_value' => 'a:7:{s:8:"gpage_id";i:' . $post_id . ';s:19:"display_page_layout";s:7:"profile";s:12:"display_page";s:6:"public";s:17:"display_page_name";s:6:"Extras";s:10:"gpage_name";s:5:"Pages";s:14:"display_gpages";s:6:"public";s:9:"home_name";s:4:"Home";}'
+    ) );
 
   sleep(2);
 }
